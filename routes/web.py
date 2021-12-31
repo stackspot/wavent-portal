c1bb55ce645790814c4526f2b23db6eb521c81c2
devjames <devjames79@gmail.com>
2021-12-31 11:59:51 +0000

    add auth routes

--- routes/web.py
+++ routes/web.py
@@ -1,3 +1,8 @@
+from masonite.authentication import Auth
 from masonite.routes import Route
 
-ROUTES = [Route.get("/", "WelcomeController@show")]
+ROUTES = [
+    Route.get("/", "WelcomeController@show"),
+]
+
+ROUTES += Auth.routes()
