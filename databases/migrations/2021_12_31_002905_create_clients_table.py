756c8fd1a9dc05ce27052f7d4d6e9d108786e524
devjames <devjames79@gmail.com>
2021-12-31 01:51:12 +0000

    create clients table

---
+++ databases/migrations/2021_12_31_002905_create_clients_table.py
@@ -0,0 +1,23 @@
+"""CreateClientsTable Migration."""
+
+from masoniteorm.migrations import Migration
+
+
+class CreateClientsTable(Migration):
+    def up(self):
+        """
+        Run the migrations.
+        """
+        with self.schema.create("clients") as table:
+            table.increments("id")
+            table.string("name", 128).nullable()
+            table.string("email", 128).nullable()
+            table.string("phone", 50).nullable()
+            table.timestamps()
+            table.soft_deletes()
+
+    def down(self):
+        """
+        Revert the migrations.
+        """
+        self.schema.drop("clients")
