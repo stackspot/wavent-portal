7b67ff44729220ada66e9782ccd13c5e6e36ff62
devjames <devjames79@gmail.com>
2021-12-31 01:51:59 +0000

    create schedules table

---
+++ databases/migrations/2021_12_31_002936_create_schedules_table.py
@@ -0,0 +1,22 @@
+"""CreateSchedulesTable Migration."""
+
+from masoniteorm.migrations import Migration
+
+
+class CreateSchedulesTable(Migration):
+    def up(self):
+        """
+        Run the migrations.
+        """
+        with self.schema.create("schedules") as table:
+            table.increments("id")
+            table.datetime("start_time")
+            table.datetime("finish_time")
+            table.timestamps()
+            table.soft_deletes()
+
+    def down(self):
+        """
+        Revert the migrations.
+        """
+        self.schema.drop("schedules")
