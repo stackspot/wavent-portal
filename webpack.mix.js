68f5828aef542333457f506536a4a1779e435942
devjames <devjames79@gmail.com>
2022-01-01 20:25:57 +0000

    remove primivue config

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,17 +3,13 @@ applications. By default, we are compiling the CSS file for the application as w
 bundling up all the JS files. */
 const mix = require('laravel-mix')
 const path = require('path')
-const AutoImport = require('unplugin-auto-import/webpack')
-const Components = require('unplugin-vue-components/webpack')
-const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 mix
 	.js('resources/js/app.js', 'storage/compiled/js')
 	.vue()
 	.sourceMaps()
 	.postCss('resources/css/app.css', 'storage/compiled/css', [
-		require('tailwindcss'),
-		require('autoprefixer'),
+		//
 	])
 
 // add an alias to js code
@@ -22,22 +18,14 @@ mix.alias({
 })
 
 mix.webpackConfig({
-	plugins: [
-		AutoImport({
-			resolvers: [ElementPlusResolver()],
-		}),
-		Components({
-			resolvers: [ElementPlusResolver()],
-		}),
-	],
 	module: {
 		rules: [
-			{
+			/* {
 				test: /\.mjs$/,
 				resolve: { fullySpecified: false },
 				include: /node_modules/,
 				type: 'javascript/auto',
-			},
+			}, */
 		],
 	},
 })
