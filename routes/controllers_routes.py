629e5e66596a291c658a0b40dd4e3fa10e4ab2d8
devjames <devjames79@gmail.com>
2022-01-05 00:17:09 +0000

    update routes

--- routes/controllers_routes.py
+++ routes/controllers_routes.py
@@ -1,77 +1,53 @@
 from masonite.routes import Route
 
-USER_ROUTES = Route.group(
-    prefix="/users",
-    name="users.",
-    routes=[
-        Route.get("", "UsersController@index").name("index"),
-        Route.post("/store", "UsersController@store").name("store"),
-        Route.get("/@user_id/profile", "UsersController@edit").name("profile"),
-        Route.put("/@user_id/update", "UsersController@update").name("update"),
-        Route.delete("/@user_id/destroy", "UsersController@delete").name("destroy"),
-        # Route.post("/@user_id/settings", "SettingsController@user_settings").name("user.settings"),
-    ],
-)
+USER_ROUTES = [
+    Route.get("", "UsersController@index").name("index"),
+    Route.get("/novo", "UsersController@create").name("create"),
+    Route.post("", "UsersController@store").name("store"),
+    Route.get("/@user_id", "UsersController@edit").name("show"),
+    Route.put("/@user_id", "UsersController@update").name("update"),
+    Route.delete("/@user_id", "UsersController@delete").name("destroy"),
+    # Route.post("/@user_id/settings", "SettingsController@user_settings").name("user.settings"),
+]
 
-ACCOUNT_ROUTES = Route.group(
-    prefix="/account",
-    name="account.",
-    routes=[
-        Route.get("", "AccountsController@index").name("index"),
-        Route.post("/store", "AccountsController@store").name("store"),
-        Route.get("/@account_id/profile", "AccountsController@edit").name("profile"),
-        Route.put("/@account_id/update", "AccountsController@update").name("update"),
-        Route.delete("/@account_id/destroy", "AccountsController@delete").name("destroy"),
-        Route.get("/settings", "SettingsController@account_settings").name("account.settings"),
-    ],
-)
+ACCOUNT_ROUTES = [
+    Route.get("", "AccountsController@index").name("index"),
+    Route.post("/store", "AccountsController@store").name("store"),
+    Route.get("/@account_id/profile", "AccountsController@edit").name("profile"),
+    Route.put("/@account_id/update", "AccountsController@update").name("update"),
+    Route.delete("/@account_id/destroy", "AccountsController@delete").name("destroy"),
+    Route.get("/settings", "SettingsController@account_settings").name("account.settings"),
+]
 
-APPOINTMENT_ROUTES = Route.group(
-    prefix="/calendar",
-    name="appointment.",
-    routes=[
-        Route.get("", "AppointmentsController@index").name("index"),
-        Route.post("/create", "AppointmentsController@store").name("store"),
-        Route.get("/@appointment_id/edit", "AppointmentsController@edit").name("edit"),
-        Route.put("/@appointment_id/update", "AppointmentsController@update").name("update"),
-        Route.delete("/@appointment_id/delete", "AppointmentsController@delete").name("delete"),
-        Route.get("/settings", "SettingsController@appointment_settings").name(
-            "appointment.settings"
-        ),
-    ],
-)
+APPOINTMENT_ROUTES = [
+    Route.get("", "AppointmentsController@index").name("index"),
+    Route.post("/create", "AppointmentsController@store").name("store"),
+    Route.get("/@appointment_id/edit", "AppointmentsController@edit").name("edit"),
+    Route.put("/@appointment_id/update", "AppointmentsController@update").name("update"),
+    Route.delete("/@appointment_id/delete", "AppointmentsController@delete").name("delete"),
+    Route.get("/settings", "SettingsController@appointment_settings").name("appointment.settings"),
+]
 
-SCHEDULE_ROUTES = Route.group(
-    prefix="/schedule",
-    name="schedule.",
-    routes=[
-        Route.get("", "SchedulesController@show").name("index"),
-        Route.post("", "SchedulesController@store").name("store"),
-        Route.get("/@schedule_id/edit", "SchedulesController@edit").name("edit"),
-        Route.put("/@schedule_id", "SchedulesController@update").name("update"),
-    ],
-)
+SCHEDULE_ROUTES = [
+    Route.get("", "SchedulesController@show").name("index"),
+    Route.post("", "SchedulesController@store").name("store"),
+    Route.get("/@schedule_id/edit", "SchedulesController@edit").name("edit"),
+    Route.put("/@schedule_id", "SchedulesController@update").name("update"),
+]
 
-SERVICE_ROUTES = Route.group(
-    prefix="/services",
-    name="service.",
-    routes=[
-        Route.get("", "ServicesController@index").name("index"),
-        Route.post("/create", "ServicesController@store").name("store"),
-        Route.get("/@service_id/edit", "ServicesController@edit").name("edit"),
-        Route.post("/@service_id/update", "ServicesController@update").name("update"),
-        Route.delete("/@service_id/delete", "ServicesController@delete").name("delete"),
-    ],
-)
-
-CLIENT_ROUTES = Route.group(
-    prefix="/clients",
-    name="client.",
-    routes=[
-        Route.get("", "ClientsController@index").name("index"),
-        Route.post("", "ClientsController@store").name("store"),
-        Route.get("/@service_id", "ClientsController@show").name("show"),
-        Route.post("/@service_id", "ClientsController@update").name("update"),
-        Route.delete("/@service_id/destroy", "ClientsController@delete").name("delete"),
-    ],
-)
+SERVICE_ROUTES = [
+    Route.get("", "ServicesController@index").name("index"),
+    Route.get("/novo", "ServicesController@create").name("create"),
+    Route.post("", "ServicesController@store").name("store"),
+    Route.get("/@service_id/editar", "ServicesController@edit").name("edit"),
+    Route.post("/@service_id", "ServicesController@update").name("update"),
+    Route.delete("/@service_id", "ServicesController@delete").name("delete"),
+]
+CLIENT_ROUTES = [
+    Route.get("", "ClientsController@index").name("index"),
+    Route.get("/novo", "ClientsController@create").name("create"),
+    Route.post("", "ClientsController@store").name("store"),
+    Route.get("/@service_id", "ClientsController@show").name("show"),
+    Route.post("/@service_id", "ClientsController@update").name("update"),
+    Route.delete("/@service_id/destroy", "ClientsController@delete").name("delete"),
+]
--- routes/web.py
+++ routes/web.py
@@ -1,8 +1,52 @@
 from masonite.authentication import Auth
 from masonite.routes import Route
 
+from .controllers_routes import (
+    ACCOUNT_ROUTES,
+    APPOINTMENT_ROUTES,
+    CLIENT_ROUTES,
+    SCHEDULE_ROUTES,
+    SERVICE_ROUTES,
+    USER_ROUTES,
+)
+
 ROUTES = [
     Route.get("/", "WelcomeController@show"),
+    Route.group(
+        [
+            Route.group(
+                USER_ROUTES,
+                prefix="/equipa",
+                name="users.",
+            ),
+            Route.group(
+                CLIENT_ROUTES,
+                prefix="/cliente",
+                name="client.",
+            ),
+            Route.group(
+                ACCOUNT_ROUTES,
+                prefix="/account",
+                name="account.",
+            ),
+            Route.group(
+                SCHEDULE_ROUTES,
+                prefix="/schedule",
+                name="schedule.",
+            ),
+            Route.group(
+                APPOINTMENT_ROUTES,
+                prefix="/calendario",
+                name="appointment.",
+            ),
+            Route.group(
+                SERVICE_ROUTES,
+                prefix="/servicos",
+                name="service.",
+            ),
+        ],
+        prefix="/admin",
+    ),
 ]
 
 ROUTES += Auth.routes()
