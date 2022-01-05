738fe8f7b0231fe08a030be3d5d6835f92287f46
devjames <devjames79@gmail.com>
2022-01-05 00:31:53 +0000

    create shared data for inertia

--- app/middlewares/HandleInertiaRequests.py
+++ app/middlewares/HandleInertiaRequests.py
@@ -1,5 +1,18 @@
+from masonite.helpers import url
 from masonite.inertia import InertiaMiddleware
+from masonite.response import Response
 
 
 class HandleInertiaRequests(InertiaMiddleware):
-    pass
+    def shared(self, request):
+        def url_back():
+            if (
+                request.get_path() != url.route("login", absolute=False)
+                and request.get_back_path() != ""
+                and request.get_back_path() != request.get_path()
+            ):
+                return request.get_back_path()
+            else:
+                return "empty"
+
+        return {"urlPrev": url_back}
