fdc8d5ef72e7a6e9db09112c8ef1a9d6374c77ac
devjames <devjames79@gmail.com>
2022-01-05 00:21:22 +0000

    style: remove h2

--- resources/js/layout/TheFooter.vue
+++ resources/js/layout/TheFooter.vue
@@ -1,12 +1,5 @@
 <template>
   <n-layout-footer>
-    <n-p>
-      <n-a href="https://github.com/zce/fearless">
-        <icon type="code"></icon>
-      </n-a>with
-      <n-text type="error">♥</n-text>by
-      <n-a href="https://zce.me">devjames</n-a>
-    </n-p>
     <n-p>&copy; {{ new Date().getFullYear() }} Stackspot. All rights reserved.</n-p>
   </n-layout-footer>
 </template>
@@ -21,8 +14,5 @@ import { Icon } from '@/components'
   opacity: 0.6;
   padding: 24px;
   text-align: center;
-}
-.n-a {
-  text-decoration: none;
 }
 </style>
