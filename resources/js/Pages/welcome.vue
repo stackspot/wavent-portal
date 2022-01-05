e0be60941934f77bdec147c71e764c42324efc31
devjames <devjames79@gmail.com>
2022-01-05 00:21:57 +0000

    change h2 text

--- resources/js/Pages/welcome.vue
+++ resources/js/Pages/welcome.vue
@@ -1,9 +1,14 @@
 <template>
-  <admin-layout></admin-layout>
+  <div>
+    <n-h2>Welcome</n-h2>
+  </div>
 </template>
 
 <script setup>
 import { AdminLayout } from '@/layout'
+import { usePage } from '@inertiajs/inertia-vue3'
+
+console.log(usePage().props.value.urlPrev)
 </script>
 
 <style>
