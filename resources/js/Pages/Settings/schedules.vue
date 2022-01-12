15d61ef23575e5158b71d87380ee713077451d26
devjames <devjames79@gmail.com>
2022-01-12 11:16:17 +0000

    refactor: change schedule layout

--- resources/js/Pages/Settings/schedules.vue
+++ resources/js/Pages/Settings/schedules.vue
@@ -1,32 +1,28 @@
 <template>
-  <n-card>
-    <n-list>
-      <n-list-item v-for="week in ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta']">
-        <n-grid :cols="12" x-gap="12">
-          <n-grid-item :span="4">
-            <n-form-item :label="week" label-placement="left">
-              <n-switch v-model:value="active">
-                <template #checked>Aberto</template>
-                <template #unchecked>Fechado</template>
-              </n-switch>
-            </n-form-item>
-          </n-grid-item>
-          <n-grid-item v-if="active" :span="8">
-            <n-space>
-              <n-time-picker
-                v-model:value="time"
-                :hours="hours"
-                :minutes="15"
-                format="HH:mm"
-                placeholder="Tempo de início"
-              />
-              <n-time-picker v-model:value="time" format="HH:mm" placeholder="Tempo de fim" />
-            </n-space>
-          </n-grid-item>
-        </n-grid>
-      </n-list-item>
-    </n-list>
-  </n-card>
+  <n-descriptions bordered>
+    <n-descriptions-item
+      v-for="week in ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta']"
+      :key="week"
+    >
+      <template #label>
+        <n-form-item :label="week" label-placement="left">
+          <n-switch v-model:value="active">
+            <template #checked>Aberto</template>
+            <template #unchecked>Fechado</template>
+          </n-switch>
+        </n-form-item>
+      </template>
+      <n-space v-if="active">
+        <n-time-picker
+          v-model:value="time"
+          :hours="schedule"
+          format="HH:mm"
+          placeholder="Tempo de início"
+        />
+        <n-time-picker v-model:value="time" format="HH:mm" placeholder="Tempo de fim" />
+      </n-space>
+    </n-descriptions-item>
+  </n-descriptions>
 </template>
 <script setup>
 import { ref } from 'vue'
@@ -34,16 +30,18 @@ import { getTime, eachHourOfInterval } from 'date-fns'
 
 const newTime = new Date()
 const active = ref(false)
-hours = []
+
+const schedule = []
+
 const allowHours = eachHourOfInterval({
   start: newTime.setHours(8),
   end: newTime.setHours(20)
 })
 
-allowHours.foreach(hours => {
-  hours.push(hours.getHours())
+allowHours.forEach(hours => {
+  schedule.push(hours.getHours())
 })
-console.log(hours)
+
 const time = ref(getTime(new Date()))
 </script>
 
