63fd44542cb94353565914094bb798779c9bff45
devjames <devjames79@gmail.com>
2022-01-16 20:10:17 +0000

    add create appointment method

--- resources/js/Pages/Appointment/index.vue
+++ resources/js/Pages/Appointment/index.vue
@@ -1,72 +1,148 @@
 <template>
-  <FullCalendar class="z-1" :options="calendarOptions" ref="calendar">
-    <template v-slot:eventContent="arg">
-      <div class="flex flex-col gap-2 p-1">
-        <b>{{ arg.timeText }}</b>
-        <i>{{ arg.event.title }}</i>
-      </div>
-    </template>
-  </FullCalendar>
+  <section ref="container">
+    <n-drawer v-model:show="showEventDrawer" :width="320" :mask-closable="false">
+      <n-drawer-content title="Criar Marcação" closable :native-scrollbar="false">
+        <n-form :model="form" :label-width="80" ref="formRef">
+          <n-form-item label="Serviço" path="service">
+            <n-select v-model:value="form.service" multiple tag clearable :options="options" />
+          </n-form-item>
+          <n-form-item label="Cliente" path="client">
+            <n-select v-model:value="form.client_id" clearable :options="options" />
+          </n-form-item>
+          <!-- <div v-show="newClient">
+            <n-form-item label="Nome do Cliente" path="client.name">
+              <n-input v-model:value="form.client.name" placeholder="Nome do Cliente" />
+            </n-form-item>
+            <n-form-item label="Email do Cliente" path="client.email">
+              <n-input v-model:value="form.client.email" placeholder="Email do Cliente" />
+            </n-form-item>
+            <n-form-item label="Nº Telemovel do Cliente" path="client.phone">
+              <n-input v-model:value="form.client.phone" placeholder="Nº Telemovel do Cliente" />
+            </n-form-item>
+          </div>-->
+          <n-form-item label="Barbeiro" path="staff">
+            <n-select v-model:value="form.staff" clearable :options="options" />
+          </n-form-item>
+          <n-form-item label="Data e Hora inicial" path="start_time">
+            <n-date-picker
+              v-model:value="form.start_time"
+              format="yyyy-MM-dd HH:mm"
+              type="datetime"
+              clearable
+              placeholder="Data e Hora inicial"
+            />
+          </n-form-item>
+          <!-- <n-form-item label="Data e Hora final" path="end_time">
+            <n-date-picker
+              v-model:value="form.start_time"
+              format="yyyy-MM-dd HH:mm"
+              type="datetime"
+              clearable
+              placeholder="Data e Hora final"
+            />
+          </n-form-item>-->
+          <n-form-item>
+            <n-space>
+              <n-button
+                type="primary"
+                @click="onCreateEvent"
+                :disabled="form.processing"
+                :loading="form.precessing"
+              >Criar Marcação</n-button>
+              <n-button @click="showEventDrawer = false">Cancelar</n-button>
+            </n-space>
+          </n-form-item>
+        </n-form>
+      </n-drawer-content>
+    </n-drawer>
+    <FullCalendar class="z-1" :options="calendarOptions" ref="calendarApp">
+      <template v-slot:eventContent="arg">
+        <n-popover class="max-w-xs">
+          <template #trigger>
+            <span class="p-1 max-h-6">
+              <n-scrollbar>
+                <div class="flex flex-col">
+                  <span class="text-sm mb-1">{{ arg.event.title }}</span>
+                  <span class="text-xs">{{ arg.timeText }}</span>
+                </div>
+              </n-scrollbar>
+            </span>
+          </template>
+          <n-thing>
+            <template #avatar>
+              <n-avatar>J</n-avatar>
+            </template>
+            <template #header>{{ arg.event.title }}</template>
+            <template #description>{{ arg.timeText }}</template>
+            <template #footer>Footer</template>
+            <template #action>
+              <n-space>
+                <n-button size="small">1$</n-button>
+              </n-space>
+            </template>
+          </n-thing>
+        </n-popover>
+      </template>
+    </FullCalendar>
+  </section>
 </template>
 
 <script setup>
 import { ref, reactive } from "vue";
+import { useForm } from '@inertiajs/inertia-vue3'
+import { getTime } from 'date-fns'
 import '@fullcalendar/core/vdom' // solve problem with Vite
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import ptLocale from '@fullcalendar/core/locales/pt';
-import { useEvents } from "@/composables"
+import { useEvents, useRoute } from "@/composables"
+import { Icon } from '@/components'
 
-let emit = defineEmits(['createEvent'])
-let events = ref([
-  {
-    start: '2022-01-09 10:35',
-    end: '2022-01-09 12:30',
-    title: 'Doctor appointment',
-  },
-  {
-    start: '2022-01-08 18:30',
-    end: '2022-01-08 19:15',
-    title: 'Dentist appointment',
-  },
-  {
-    start: '2022-01-03 18:30',
-    end: '2022-01-03 19:30',
-    title: 'Crossfit',
-  },
-  {
-    start: '2022-01-04 12:00',
-    end: '2022-01-04 13:00',
-    title: 'Brunch with Jane',
-  },
-  {
-    start: '2022-01-05 19:00',
-    end: '2022-01-05 19:45',
-    title: 'Swimming lesson',
-  },
-  {
-    start: '2022-01-07 19:00',
-    end: '2022-01-07 19:45',
-    title: 'Swimming lesson',
-  },
-  {
-    start: '2022-01-06 12:00',
-    end: '2022-01-06 14:00',
-    title: 'LUNCH',
-    class: 'lunch',
-    background: true,
-  },
-  {
-    start: '2022-01-07 12:00',
-    end: '2022-01-07 14:00',
-    title: 'LUNCH',
-    class: 'lunch',
-    background: true,
-  },
-])
 
+let { events } = useEvents()
+const { route } = useRoute()
+
+const calendarApp = ref()
+const showEventDrawer = ref(false)
+const newClient = ref(false)
+const form = useForm({
+  service: null,
+  staff: null,
+  client_id: null,
+  client: {
+    name: null,
+    email: null,
+    phone: null
+  },
+  start_time: null,
+  finish_time: null,
+})
+
+const options = ref([
+  {
+    label: 'Drive My Car',
+    value: 'song1'
+  },
+  {
+    label: 'Norwegian Wood',
+    value: 'song2'
+  },
+  {
+    label: "You Won't See",
+    value: 'song3',
+    disabled: true
+  }])
+
+
+const onSelectDate = (arg) => {
+  const cal = arg.view.calendar
+  cal.unselect()
+  form.start_time = getTime(arg.start)
+  form.finish_time = getTime(arg.end)
+  showEventDrawer.value = true
+}
 
 let calendarOptions = reactive({
   plugins: [
@@ -74,29 +150,21 @@ let calendarOptions = reactive({
     timeGridPlugin,
     interactionPlugin // needed for dateClick
   ],
-  customButtons: {
-    createEvent: {
-      text: 'Criar Marcação',
-      click: function () {
-        emit('createEvent')
-      }
-    },
-  },
   headerToolbar: {
     left: 'prev,next today',
     center: 'title',
-    right: 'createEvent timeGridWeek,timeGridDay'
+    right: 'timeGridWeek,timeGridDay'
   },
   locale: ptLocale,
   initialView: 'timeGridWeek',
-  initialEvents: events.value, // alternatively, use the `events` setting to fetch from a feed
+  initialEvents: events, // alternatively, use the `events` setting to fetch from a feed
   editable: true,
   selectable: true,
   selectMirror: true,
   dayMaxEvents: true,
   weekends: true,
-  /* select: handleDateSelect,
-  eventClick: handleEventClick,
+  select: onSelectDate,
+  /*eventClick: handleEventClick,
   eventsSet: handleEvents, */
   /* you can update a remote database when these fire:
   eventAdd:
@@ -109,9 +177,18 @@ let calendarOptions = reactive({
   slotMaxTime: '20:30:00',
   slotDuration: '00:15:00',
   nowIndicator: true,
-  businessHours: true,
-  eventColor: "#ffa101",
+  businessHours: true
 })
+
+console.log(calendarApp.value)
+const onCreateEvent = async () => {
+  form.post(route('appointment.store'), {
+    onSuccess: () => {
+      showEventDrawer.value = false
+    }
+  })
+}
+
 
 </script>
 
