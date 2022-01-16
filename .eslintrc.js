6ed3738fdd1409536f7c14652a1a20e36136b58c
devjames <devjames79@gmail.com>
2022-01-16 20:11:26 +0000

    add balm ui config

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	root: true,
+	parserOptions: {
+		parser: 'babel-eslint',
+		ecmaVersion: 2020,
+		sourceType: 'module',
+	},
+	plugins: ['eslint-plugin-prettier', 'eslint-plugin-vue'],
+}
