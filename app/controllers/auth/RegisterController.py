4ee5f1ca22b83025f7cd78cda4eb0b192247b6f3
devjames <devjames79@gmail.com>
2022-01-16 20:06:24 +0000

    change register controller

--- app/controllers/auth/RegisterController.py
+++ app/controllers/auth/RegisterController.py
@@ -1,21 +1,23 @@
+from app.models.Account import Account
+from masonite.authentication import Auth
 from masonite.controllers import Controller
-from masonite.views import View
+from masonite.inertia import Inertia
 from masonite.request import Request
 from masonite.response import Response
-from masonite.authentication import Auth
+from masonite.views import View
+from slugify import slugify
 
 
 class RegisterController(Controller):
-    def show(self, view: View):  # Show register page
-        return view.render("auth.register")
+    def show(self, view: Inertia):  # Show register page
+        return view.render("Auth/register")
 
-    def store(
-        self, auth: Auth, request: Request, response: Response
-    ):  # store register user
+    def store(self, auth: Auth, request: Request, response: Response):  # store register user
         errors = request.validate(
             {
                 "email": "required",
                 "name": "required",
+                "phone": "required",
                 "password": "required|strong|confirmed",
             }
         )
@@ -23,9 +25,11 @@ class RegisterController(Controller):
         if errors:
             return response.back().with_errors(errors)
 
-        user = auth.register(request.only("name", "email", "password"))
+        user = auth.register(request.only("name", "email", "password", "phone"))
+        if user:
 
+            Account.create({"name": auth.user().name, "slug": slugify(auth.user().name)})
         if not user:
             return response.redirect("/register")
 
-        return response.redirect("/home")
+        return response.redirect(name="dashboard")
