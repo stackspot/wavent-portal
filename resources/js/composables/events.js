0128e467f8d2f0a5745a927d88b4cdf2903c1215
devjames <devjames79@gmail.com>
2022-01-16 19:59:32 +0000

    update events composable

--- resources/js/composables/events.js
+++ resources/js/composables/events.js
@@ -1,52 +1,73 @@
 import { createSharedComposable } from '@vueuse/core'
+import { reactive } from 'vue'
+import { Inertia } from '@inertiajs/inertia'
+import { useRoute } from './route'
 
-export default createSharedComposable(function useEvents() {
-	let events = ref([
-		{
-			start: '2021-12-22 10:35',
-			end: '2021-12-22 12:30',
-			title: 'Doctor appointment',
-		},
-		{
-			start: '2021-12-22 18:30',
-			end: '2021-12-22 19:15',
-			title: 'Dentist appointment',
-		},
-		{
-			start: '2021-12-23 18:30',
-			end: '2021-12-23 19:30',
-			title: 'Crossfit',
-		},
-		{
-			start: '2021-12-24 12:00',
-			end: '2021-12-24 13:00',
-			title: 'Brunch with Jane',
-		},
-		{
-			start: '2021-12-25 19:00',
-			end: '2021-12-25 19:45',
-			title: 'Swimming lesson',
-		},
-		{
-			start: '2021-12-30 19:00',
-			end: '2021-12-30 19:45',
-			title: 'Swimming lesson',
-		},
-		{
-			start: '2021-12-26 12:00',
-			end: '2021-12-26 14:00',
-			title: 'LUNCH',
-			class: 'lunch',
-			background: true,
-		},
-		{
-			start: '2021-12-27 12:00',
-			end: '2021-12-27 14:00',
-			title: 'LUNCH',
-			class: 'lunch',
-			background: true,
-		},
-	])
+let events = reactive([
+	{
+		start: '2022-01-12 10:35',
+		end: '2022-01-12 12:30',
+		title: 'Doctor appointment',
+	},
+	{
+		start: '2022-01-12 18:30',
+		end: '2022-01-12 19:15',
+		title: 'Dentist appointment',
+	},
+	{
+		start: '2022-01-13 18:30',
+		end: '2022-01-13 19:30',
+		title: 'Crossfit',
+	},
+	{
+		start: '2022-01-14 12:00',
+		end: '2022-01-14 13:00',
+		title: 'Brunch with Jane',
+	},
+	{
+		start: '2022-01-15 19:00',
+		end: '2022-01-15 19:45',
+		title: 'Swimming lesson',
+	},
+	{
+		start: '2022-01-10 19:00',
+		end: '2022-01-10 19:45',
+		title: 'Swimming lesson',
+	},
+	{
+		start: '2022-01-16 12:00',
+		end: '2022-01-16 14:00',
+		title: 'LUNCH',
+		class: 'lunch',
+		background: true,
+	},
+	{
+		start: '2022-01-17 12:00',
+		end: '2022-01-17 14:00',
+		title: 'LUNCH',
+		class: 'lunch',
+		background: true,
+	},
+])
 
-	return { events }
+const { route } = useRoute()
+
+export const useEvents = createSharedComposable(() => {
+	const getEvents = () => {
+		const appointments = Inertia.get(route('appointment.ajax'))
+		return appointments
+	}
+	const setEvents = () => {}
+	const createEvent = (appointment) => {
+		Inertia.post(route('appointment.store'), appointment)
+	}
+
+	const updateEvent = (appointment_id) => {
+		Inertia.post(route('appointment.update'), appointment_id)
+	}
+
+	const removeEvent = (appointment_id) => {
+		Inertia.post(route('appointment.delete'), appointment_id)
+	}
+	return { events, createEvent, updateEvent, removeEvent }
 })
