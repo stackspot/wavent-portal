c8e058a9cae99882310a12881702b7a65b625477
devjames <devjames79@gmail.com>
2022-01-17 18:24:59 +0000

    add user account prop

--- app/controllers/WelcomeController.py
+++ app/controllers/WelcomeController.py
@@ -1,11 +1,12 @@
 """A WelcomeController Module."""
-from masonite.views import View
 from masonite.controllers import Controller
 from masonite.inertia import Inertia
+from masonite.request import Request
+from masonite.views import View
 
 
 class WelcomeController(Controller):
     """WelcomeController Controller Class."""
 
-    def show(self, view: Inertia):
-        return view.render("welcome")
+    def show(self, view: Inertia, request: Request):
+        return view.render("welcome", {"user_account": request.user().account.serialize()})
