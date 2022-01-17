0af839198ed24dc018b06161b7b497645331ac59
devjames <devjames79@gmail.com>
2022-01-17 18:24:26 +0000

    change redirect route to dashboard  on login successfuly

--- app/controllers/auth/LoginController.py
+++ app/controllers/auth/LoginController.py
@@ -1,21 +1,20 @@
+from masonite.authentication import Auth
 from masonite.controllers import Controller
-from masonite.views import View
+from masonite.inertia import Inertia
 from masonite.request import Request
 from masonite.response import Response
-from masonite.authentication import Auth
+from masonite.views import View
 
 
 class LoginController(Controller):
-    def show(self, view: View):
-        return view.render("auth.login")
+    def show(self, view: Inertia):
+        return view.render("Auth/login")
 
     def store(self, view: View, request: Request, auth: Auth, response: Response):
-        login = auth.attempt(request.input("username"), request.input("password"))
+        login = auth.attempt(request.input("email"), request.input("password"))
 
         if login:
-            return response.redirect(name="auth.home")
+            return response.redirect(name="dashboard")
 
         # Go back to login page
-        return response.redirect(name="login").with_errors(
-            ["The email or password is incorrect"]
-        )
+        return response.redirect(name="login").with_errors(["The email or password is incorrect"])
