2699ad6cc5ad9836027c9f49718d4dfc0906ce36
devjames <devjames79@gmail.com>
2022-01-18 12:28:01 +0000

    fix: errors methods given json->messagebag error

--- app/middlewares/HandleInertiaRequests.py
+++ app/middlewares/HandleInertiaRequests.py
@@ -11,14 +11,7 @@ class HandleInertiaRequests(InertiaMiddleware):
         else:
             return {}
 
-    def get_errors(self, session):
-        if not isinstance(session.get("errors"), str) and session.has("errors"):
-            return session.get("errors").json()
-        else:
-            return {}
-
     def share(self, request):
         session = request.app.make("session")
-        errors = self.get_errors(session)
         success = self.get_success(session)
-        return {"errors": errors, "success": success}
+        return {"success": success}
