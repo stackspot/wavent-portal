20acf92effd85f73b3a5c5a242500991735f47e0
devjames <devjames79@gmail.com>
2022-01-23 00:25:19 +0000

    setup compnay create process

--- resources/js/Pages/Settings/companyInfo.vue
+++ resources/js/Pages/Settings/companyInfo.vue
@@ -1,29 +1,80 @@
 <template>
-  <n-thing content-indented>
-    <template #avatar>
-      <n-avatar></n-avatar>
-    </template>
-    <template #header>Company name</template>
-    <template #description>Description</template>
-    Money is any item or verifiable record that is generally accepted as payment
-    for goods and services and repayment of debts, such as taxes, in a particular
-    country or socio-economic context.
-    <template
-      #footer
-    >Footer</template>
-    <template #action>
-      <n-space>
-        <n-button size="small">10$</n-button>
-        <n-button size="small">100$</n-button>
-      </n-space>
-    </template>
-  </n-thing>
+  <n-form :model="company" ref="formRef" label-placement="top" class="max-w-md md:mx-auto">
+    <n-grid cols="12" responsive="screen" :x-gap="24">
+      <n-form-item-gi :span="12" label="Nome" path="name">
+        <n-input placeholder="Nome" v-model:value="company.name" />
+      </n-form-item-gi>
+      <n-form-item-gi :span="12" label="Email" path="email">
+        <n-input type="email" placeholder="Email" v-model:value="company.email" />
+      </n-form-item-gi>
+      <n-form-item-gi :span="12" label="Nº telemovel" path="phone">
+        <n-input type="tel" placeholder="Nº Telemovel" v-model:value="company.phone" />
+      </n-form-item-gi>
+      <n-form-item-gi :span="12" label="Endereço" path="address">
+        <n-input placeholder="Endereço" v-model:value="company.address" />
+      </n-form-item-gi>
+      <n-form-item-gi :span="12" label="Descrição" path="description">
+        <n-input
+          type="textarea"
+          placeholder="Breve descrição da empresa"
+          v-model:value="company.description"
+        />
+      </n-form-item-gi>
+    </n-grid>
+    <n-space>
+      <n-button type="primary" @click="createProvider">Guardar</n-button>
+    </n-space>
+  </n-form>
 </template>
 
-<script>
-export default {
+<script setup>
+import { ref, onBeforeMount } from 'vue'
+import { Inertia } from '@inertiajs/inertia'
+import axios from 'axios'
+import { useRoute } from '@/composables'
 
+const logoUpdalod = ref()
+const brandUpdalod = ref()
+const provider = ref(null)
+const { route } = useRoute()
+
+const company = ref({
+  name: null,
+  email: null,
+  phone: null,
+  address: null,
+  description: null,
+  logo: null,
+  brand_image: null,
+})
+
+const getProvider = async () => {
+  const response = await axios.get(route('provider.index'))
+  provider.value = response.data.provider
+  company.value = provider.value
 }
+
+/* const toBase64 = file => new Promise((resolve, reject) => {
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = () => resolve(reader.result);
+  reader.onerror = error => reject(error);
+})
+
+const handleLogoChange = async ({ file }) => {
+  company.value.logo = await toBase64(file.file)
+}
+const handleBrandChange = async ({ file }) => {
+  company.value.brand_image = await toBase64(file.file)
+} */
+const createProvider = () => {
+  Inertia.post(route('provider.store'), company.value, {
+    onSuccess: () => getProvider(),
+    onError: (e) => console.log(e)
+  })
+}
+
+onBeforeMount(() => getProvider())
 </script>
 
 <style>
