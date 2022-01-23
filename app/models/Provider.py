bcbc057e1c182a29528d504c10ca2d34fac805b1
devjames <devjames79@gmail.com>
2022-01-23 00:23:19 +0000

    add visible privite attribute and logo_path

--- app/models/Provider.py
+++ app/models/Provider.py
@@ -1,12 +1,15 @@
 """ Provider Model """
 
+from masonite.helpers import url
 from masoniteorm.models import Model
 from masoniteorm.relationships import belongs_to
 from masoniteorm.scopes import SoftDeletesMixin
 
 
-class Provider(Model, SoftDeletesMixin):
+class Provider(Model):
     """Provider Model"""
+
+    __appends__ = ["logo_path"]
 
     __fillable__ = [
         "name",
@@ -18,9 +21,22 @@ class Provider(Model, SoftDeletesMixin):
         "brand_image",
         "account_id",
     ]
+    __visible__ = [
+        "name",
+        "email",
+        "phone",
+        "address",
+        "description",
+        "logo_path",
+        "brand_image",
+        "account_id",
+    ]
 
     @belongs_to("account_id", "id")
     def account(self):
         from app.models.Account import Account
 
         return Account
+
+    def get_logo_path_attribute(self):
+        return f"/uploads/{self.logo}"
